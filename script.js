91a83d94fddcb872dd2ac2e2c873a696f24e9b9d
Ivan Shorstov <shorstov.ivan@gmail.com>
2024-12-08 21:21:38 +0300

    Проект готов

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const ROCK = 'rock';
-const SCISSORS = 'sciccors';
-const PAPPER = 'papper';
 const imgs = document.querySelectorAll(".bottom-container img");
 const computerSelection = getComputerChoice();
 
@@ -8,50 +5,102 @@ const computerSelection = getComputerChoice();
 let humanScore = 0;
 let computerScore = 0;
 
+if (humanScore === 5 || computerScore === 5) {
+    humanScore = 0;
+    computerScore = 0;
+    document.querySelector(".you-score").textContent = `${humanScore}`;
+    document.querySelector(".computer-score").textContent = `${computerScore}`;
+};
+
 
 function getComputerChoice() {
     const random = (min = 1, max = 4) => {
-        return Math.floor(Math.random() * (max - min) + min);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
-    if (random() === 1) return ROCK;
-    else if (random() === 2) return SCISSORS;
-    else return PAPPER;
+    if (random() === 1) return 'rock';
+    else if (random() === 2) return 'scissors';
+    else return 'papper';
+};
 
-}
 
-function playRound(humanChoice, computerChoice) {
-    
+imgs.forEach(img => img.addEventListener('click', () => {
+
+    const compChoice = getComputerChoice();
+    const roundResult = playRound(img.id, compChoice);
+    document.querySelector(".central-container").textContent = `${roundResult}`;
+
+    const compImg = document.querySelector("#comp-choice-img");
+    compImg.setAttribute("src", `/images/${compChoice}.png`);
+
+    document.querySelector(".you-score").textContent = `${humanScore}`;
+    document.querySelector(".computer-score").textContent = `${computerScore}`;
+
+    if (humanScore === 5 || computerScore === 5){
+        const popUp = document.querySelector(".result-alert-container");
+        popUp.classList.add("result-alert-container-visible");
+        
+        const resultSign = document.querySelector(".result-sign");
+        resultSign.classList.add("result-sign-anim");
+
+        const paraRes = document.querySelector(".p-result");
+        const replayBtn = document.querySelector(".btn-play");
+
+        if (humanScore === 5){
+            paraRes.textContent = "You have beaten the computer!";
+        } else {
+            paraRes.textContent = "You have been beaten by the computer!";
+        }
+
+        replayBtn.addEventListener("click", () => {
+        humanScore = 0;
+        computerScore = 0;
+        document.querySelector(".you-score").textContent = `${humanScore}`;
+        document.querySelector(".computer-score").textContent = `${computerScore}`;
+        popUp.classList.remove("result-alert-container-visible");
+        resultSign.classList.remove("result-sign-anim");
+        document.querySelector(".central-container").textContent = "Choose yor object!";
+        compImg.setAttribute("src", ``)
+
+        });
+
+    }
+
+}));
+
+
+function playRound(humanChoice, computerSelection) {
+
     let result;
 
-    if (humanChoice === 'rock' && computerChoice === 'rock') {
+    if (humanChoice === 'rock' && computerSelection === 'rock') {
         return result = "It's a draw";
-    } else if (humanChoice === 'rock' && computerChoice === 'sciccors') {
-        result++;
+    } else if (humanChoice === 'rock' && computerSelection === 'scissors') {
+        humanScore++;
         return result = "This is a victory";
-    } else if (humanChoice === 'rock' && computerChoice === 'papper') {
-        computerChoice++;
+    } else if (humanChoice === 'rock' && computerSelection === 'papper') {
+        computerScore++;
         return result = "You've lost";
 
-    } if (humanChoice === 'sciccors' && computerChoice === 'rock') {
-        computerChoice++;
+    } if (humanChoice === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
         return result = "You've lost";
-    } else if (humanChoice === 'sciccors' && computerChoice === 'sciccors') {
+    } else if (humanChoice === 'scissors' && computerSelection === 'scissors') {
         return result = "It's a draw";
-    } else if (humanChoice === 'sciccors' && computerChoice === 'papper') {
-        humanChoice++;
+    } else if (humanChoice === 'scissors' && computerSelection === 'papper') {
+        humanScore++;
         return result = "This is a victory";
 
-    } if (humanChoice === 'papper' && computerChoice === 'rock') {
-        humanChoice++;
+    } if (humanChoice === 'papper' && computerSelection === 'rock') {
+        humanScore++;
         return result = "This is a victory";
-    } else if (humanChoice === 'papper' && computerChoice === 'sciccors') {
-        computerChoice++;
+    } else if (humanChoice === 'papper' && computerSelection === 'scissors') {
+        computerScore++;
         return result = "You've lost";
-    } else if (humanChoice === 'papper' && computerChoice === 'papper') {
+    } else if (humanChoice === 'papper' && computerSelection === 'papper') {
         return result = "It's a draw";
     }
 
-}
+};
 
 
 
